11ff7e62fbf5b12054cf9187dbfc49b69f453408
Marcelo Cícero Rodrigues da Costa <marciro@gmail.com>
2021-09-25 20:29:07 -0300

    Initial commit

---
+++ src/main/java/br/org/mcr/ssoexemplo/SsoexemploApplication.java
@@ -0,0 +1,13 @@
+package br.org.mcr.ssoexemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoexemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoexemploApplication.class, args);
+	}
+
+}
